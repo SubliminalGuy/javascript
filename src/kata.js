694a366aa0863ea5d18f017bace866405712498d
Subliminal Guy <subliminal_kid@posteo.de>
2023-11-04 16:20:50 +0100

    Add Passing Test

--- src/kata.js
+++ src/kata.js
@@ -4,16 +4,16 @@ const createRandomArray = (x, y) => {
     let randomArray = Array.from(Array(y), () => (Math.random() > 0.5 ? 1 : 0));
     grid.push(randomArray);
   }
-  console.log(grid);
+
   return grid;
 };
 
 const sumArray = (myArray) => {
   let sum = 0;
-
   // iterate over each item in the array
-  for (let j = 0; j < myArray[0]; j++) {
-    sum += myArray[j];
+  for (let j = 0; j < myArray[0].length; j++) {
+    console.log(myArray[0][j]);
+    sum += myArray[0][j];
   }
   console.log(sum);
   return sum;
