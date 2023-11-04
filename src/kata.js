9c309e6f007b361a70b19291572d3b0b3fc71104
Subliminal Guy <subliminal_kid@posteo.de>
2023-11-04 16:01:07 +0100

    Functionalize createZeroArray

--- src/kata.js
+++ src/kata.js
@@ -1,9 +1,9 @@
-const createZeroArray = () => {
-  return [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
+const createZeroArray = (x, y) => {
+  let grid = [];
+  for (let i = 0; i < x; i++) {
+    grid.push([0, 0, 0]);
+  }
+  return grid;
 };
 
 module.exports = { createZeroArray };
--- test/kata.test.js
+++ test/kata.test.js
@@ -2,7 +2,7 @@ const { createZeroArray } = require("../src/kata");
 
 describe("Kata", function () {
   it("array is created", function () {
-    var result = createZeroArray();
+    var result = createZeroArray(3, 3);
     expect(result.length).toBe(3);
   });
 });
